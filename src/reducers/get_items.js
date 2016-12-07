4ef9f19808e72c7bbf726b5151d7ac5cdbf2b4a7
connor <connorpeshek@gmail.com>
2016-12-07 11:14:06 -0500

    Add comments to state.

--- src/reducers/get_items.js
+++ src/reducers/get_items.js
@@ -1,13 +1,13 @@
 import {NEW_STORIES, ITEM} from '../actions/index'
 
-const initialState = {items: null, item: null}
+const initialState = {items: null, item: null, comments: null}
 
 export default function(state = initialState, action) {
   switch(action.type) {
     case NEW_STORIES:
       return {...state, items: action.payload}
     case ITEM:
-      return {...state, item: action.payload}
+      return {...state, item: action.payload, comments: action.comments}
   }
   return state
 }
