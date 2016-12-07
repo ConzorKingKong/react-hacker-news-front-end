6df98e8edbabb2f5ec5ef1aa45f4fcbaf469e6f2
connor <connorpeshek@gmail.com>
2016-12-07 11:13:44 -0500

    Add comments to state

--- src/reducers/get_user.js
+++ src/reducers/get_user.js
@@ -1,11 +1,11 @@
 import {USER} from '../actions/index'
 
-const initialState = {user: null}
+const initialState = {user: null, comments: null}
 
 export default (state = initialState, action) => {
   switch(action.type) {
     case USER:
-      return {...state, user: action.payload.data}
+      return {...state, user: action.payload, comments: action.comments}
     default:
       return state
   }
