7cb0279f7fb121bfa84326a90a067d9d36077716
connor <connorpeshek@gmail.com>
2016-12-10 17:19:37 -0500

    Fix link tags.

--- src/components/app.js
+++ src/components/app.js
@@ -6,10 +6,10 @@ export default (props) => {
     <div className="wrapper">
       <div className="title-bar">
         <Link to="/">New Stories</Link>
-        <Link to="topstories">Top Stories</Link>
-        <Link to="show">Show</Link>
-        <Link to="ask">Ask</Link>
-        <Link to="jobs">Jobs</Link>
+        <Link to="/topstories">Top Stories</Link>
+        <Link to="/show">Show</Link>
+        <Link to="/ask">Ask</Link>
+        <Link to="/jobs">Jobs</Link>
       </div>
       <div className="content">
         {props.children}
