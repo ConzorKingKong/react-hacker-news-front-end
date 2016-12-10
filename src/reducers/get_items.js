305f56f1a7d70adc457a451074622d24d1c5b72f
connor <connorpeshek@gmail.com>
2016-12-10 17:17:38 -0500

    Add clear state functionality.

--- src/reducers/get_items.js
+++ src/reducers/get_items.js
@@ -1,4 +1,4 @@
-import {NEW_STORIES, ITEM} from '../actions/index'
+import {NEW_STORIES, ITEM, CLEAR_STORIES, CLEAR_ITEM} from '../actions/index'
 
 const initialState = {items: null, item: null, comments: null}
 
@@ -6,8 +6,12 @@ export default function(state = initialState, action) {
   switch(action.type) {
     case NEW_STORIES:
       return {...state, items: action.payload}
+    case CLEAR_STORIES:
+      return {...state, items: action.payload}
     case ITEM:
       return {...state, item: action.payload, comments: action.comments}
+    case CLEAR_ITEM:
+      return {...state, item: action.payload}
   }
   return state
 }
