ad66f05ed34a2cc5958ef8f16e79d95dacb3fa09
connor <connorpeshek@gmail.com>
2016-12-15 16:52:12 -0500

    Add spaces per StandardJs

--- app.js
+++ app.js
@@ -9,41 +9,41 @@ app.use('/item', express.static('public'))
 app.use('/comment', express.static('public'))
 app.use('/stories', express.static('public'))
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/ask', function(req, res) {
+app.get('/ask', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/jobs', function(req, res) {
+app.get('/jobs', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/topstories', function(req, res) {
+app.get('/topstories', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/show', function(req, res) {
+app.get('/show', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/item/:id', function(req, res) {
+app.get('/item/:id', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/stories/:id', function(req, res) {
+app.get('/stories/:id', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/user/:id', function(req, res) {
+app.get('/user/:id', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/comments/:id', function(req, res) {
+app.get('/comments/:id', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(port)
-console.log("booted on port " + port)
+console.log('booted on port ' + port)
