9663a1f97b4c09bfea2e9ec2810fdba3778f7ddd
connor <connorpeshek@gmail.com>
2017-01-09 23:47:27 -0500

    Update mobile styling.

--- src/components/algolia-post/algolia-post.js
+++ src/components/algolia-post/algolia-post.js
@@ -8,12 +8,16 @@ export default (props) => {
   date.setUTCSeconds(utcSeconds)
   return (
     <div key={props.id} className='posts-list-item'>
-      <div className='posts-list-item-score'>{props.score}</div>
-      <Link className='posts-list-item-title' to={`item/${props.id}`}>{props.title}</Link>
-      <div className='posts-list-item-by'>by</div>
-      <Link className='posts-list-item-user' to={`user/${props.by}`}>{props.by}</Link>
-      <div className='posts-list-item-date' title={date}>
-        <FormattedRelative value={date} />
+      <div className='posts-list-header'>
+        <div className='posts-list-item-score'>{props.score}</div>
+        <Link className='posts-list-item-title' to={`item/${props.id}`}>{props.title}</Link>
+      </div>
+      <div className='posts-list-item-subtitle'>
+        <div className='posts-list-item-by'>by</div>
+        <Link className='posts-list-item-user' to={`user/${props.by}`}>{props.by}</Link>
+        <div className='posts-list-item-date' title={date}>
+          <FormattedRelative value={date} />
+        </div>
       </div>
     </div>
   )
