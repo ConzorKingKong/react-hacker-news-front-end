f674bc69873ee1d26e86b652abefd3b837d5381b
connor <connor@connors-MacBook-Pro.local>
2025-06-18 11:38:38 -0500

    Fix imports.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require('express')
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express()
-const path = require("path")
 
 app.use(express.static('dist'))
 
